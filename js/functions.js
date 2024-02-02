41f4e3d3bec857d5cc2f8d1f33eeba839d6ca966
kiakauppi <n2kaki00@students.oamk.fi>
2024-02-02 11:15:08 +0200

    Tehtävä tehty loppuun

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+const div = document.querySelector('div')
+const img = document.querySelector('img')
+
+const getRandomIntNumberInRange = (min,max) => {
+return Math.floor(Math.random()*max)+min
+}
+
+document.querySelector('#dice').addEventListener('click', () =>{
+    const randomized_number = getRandomIntNumberInRange(1,6)
+
+    document.querySelector('#dice img').src = './img/' + randomized_number + '.png'
+})
